102ca767cb740b048b683e6d2c84a3b52cfce1ad
mavenor <shreedh@outlook.com>
2022-11-13 23:52:29 +0530

    Face: update for PiCamera2

--- _face.py
+++ _face.py
@@ -1,40 +1,50 @@
-import picamera
+import picamera2 as cam
 from time import time
 from face_recognition import face_encodings as encode, compare_faces, face_locations, load_image_file as imgread
 from numpy import empty as newarr, uint8, NDArray
 
 class facemon:
+    # camera object
+    camera: cam.Picamera2
+
     # face memory
     face0: NDArray
-
-    camera = picamera.PiCamera()
-    camera.resolution = (320, 240)
 
     # most recent frame containing a face
     # used when face has just been discovered using has_face()
     recent_positive_frame: NDArray
 
-    frame = newarr((240, 320, 3), dtype=uint8)
+    def __init__(self):
+        self.camera = cam.Picamera2()
+        self.camera.still_configuration.main.size = (1296, 972)
+        self.camera.configure("still")
+        self.camera.start()
+        self.recent_positive_frame = None
 
     # store most recent frame with face into face memory
     def store_face(self):
+        if self.recent_positive_frame is None:
+            self.recent_positive_frame = self.camera.capture_array("main")
         self.face0 = encode(self.recent_positive_frame)[0]
 
     # check if 
     def match_face(self):
-        self.camera.capture(self.frame, format="rgb")
+        self.frame = self.camera.capture_array("main")
         face1 = encode(self.frame)[0]
         return compare_faces([self.face0], face1)
 
     # face is present in stream?
-    def has_face(self):
+    def has_face(self) -> bool:
         start = time()
         total = 0
         positve = 0
         while (time() - start < 10):
-            if (len(face_locations(self.frame)) == 1):
+            frame = self.camera.capture_array("main")
+            # only one face should be in the frame
+            if (len(face_locations(frame)) == 1):
                 positve += 1
-        total += 1
+                self.recent_positive_frame = frame
+            total += 1
 
         # use smoothing for false positives
         return (float(positve)/float(total) >= 0.1)
