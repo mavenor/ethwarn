7d0e3a9bcc1f85d0ea3ec936017164b4c71c1f3f
mavenor <shreedh@outlook.com>
2022-11-14 07:10:28 +0530

    Face: change matching method to distance

--- _face.py
+++ _face.py
@@ -1,44 +1,53 @@
 import picamera2 as cam
 from time import time
-from face_recognition import face_encodings as encode, compare_faces, face_locations, load_image_file as imgread
+from face_recognition import face_encodings as encode, face_distance as dist, face_locations, load_image_file as imgread
 from numpy import empty as newarr, uint8, NDArray
+
 
 class facemon:
     # camera object
     camera: cam.Picamera2
 
     # face memory
-    face0: NDArray
+    # (EMBEDDINGS)
+    face0: list[NDArray]
 
     # most recent frame containing a face
     # used when face has just been discovered using has_face()
+    # (RAW RGB DATA)
     recent_positive_frame: NDArray
 
     def __init__(self):
         self.camera = cam.Picamera2()
         self.camera.still_configuration.main.size = (1296, 972)
+        self.camera.still_configuration.main.format = "RGB888"
         self.camera.configure("still")
         self.camera.start()
         self.recent_positive_frame = None
+        self.face0 = None
 
     # store most recent frame with face into face memory
     def store_face(self):
         if self.recent_positive_frame is None:
-            self.recent_positive_frame = self.camera.capture_array("main")
-        self.face0 = encode(self.recent_positive_frame)[0]
+            self.wait_face()
+        self.face0.append(encode(self.recent_positive_frame)[0])
+
+    def reset_face(self):
+        self.face0.clear()
 
     # check if 
     def match_face(self):
         self.frame = self.camera.capture_array("main")
         face1 = encode(self.frame)[0]
-        return compare_faces([self.face0], face1)
+        distance = dist(self.face0, face1)
+        return (distance.mean() >= 0.4)
 
     # face is present in stream?
     def has_face(self) -> bool:
         start = time()
         total = 0
         positve = 0
-        while (time() - start < 10):
+        while (time() - start < 6):
             frame = self.camera.capture_array("main")
             # only one face should be in the frame
             if (len(face_locations(frame)) == 1):
@@ -55,13 +64,3 @@ class facemon:
             if self.has_face():
                 break
         # face detected once outside loop
-
-
-def single_face(image):
-    img = imgread(image)
-    return encode(img)[0]
-
-def is_match(img1, img2):
-    face1 = single_face(img1)
-    face2 = single_face(img2)
-    return compare_faces([face1], face2)
